11509ddeb4c8eb8030884cb32d30a2649bd8a2b8
deepukumar01 <111313467+deepukumar01@users.noreply.github.com>
2022-08-25 21:07:04 +0530

    Create Assignment-1 : Warm-Up with Python.py
    This is the Complete Solution for "Assignment-1 : Warm-Up with Python" by Deepu Kumar.

---
+++ Assignment-1 : Warm-Up with Python.py
@@ -0,0 +1,57 @@
+#1. Write a command to get the Python version you are using.
+Ans) If you wanna check your Python version, follow these steps-
+     1. Open Command Prompt (cmd) 
+     2. Type "python" or "py" in the cmd shell and click Enter
+     You will get the required Python Version.
+
+
+#2. Write a python script to print Hello Python on the screen directly in the command line.
+Ans) If you wanna print Hello Python on the screen directly in the command line, follow these steps-
+     1. Open Command Prompt (cmd) 
+     2. Type "python" or "py" in the cmd shell and click Enter
+     3. write python("Hello Python") & click Enter
+     You will get the required result.
+     
+
+#3. Write a python script and store it in a file and execute the file from the command line.
+Ans) First, we have to write our code & save it with .py extension.
+     Let we have created xyz.py file
+     Now follow the below steps to run it from the command line-
+     1. Open Command Prompt (cmd) 
+     2. Go to the exact directory where your xyz.py file is stored
+     3. then write python xyz.py in the shell
+     & that's it, Your python file executed successfully from command line.
+
+
+#4. Write a python script to print Hello Python on the screen using visual studio code.
+Ans) print("Hello Python")
+
+
+#5. Write a python script to print Hello on the first line and Python on the second line.
+Ans) print("Hello")
+     print("Python")
+            or
+     print("Hello\nPython")
+
+
+#6. Write a python script to print “MySirG” on the screen.
+Ans) print("\"MySirG\"")
+
+
+#7. Write a python script to print “Teacher’s Day” on the screen.
+Ans) print("\"Teacher's Day\"")
+
+
+#8. Write a python script to print the value of a variable. Variable contains your name as data.
+Ans) Name="Deepu Kumar"
+     print(Name)
+
+
+#9. Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+Ans) Name,Age,Qual,YoE="Deepu Kumar",22,"B.Tech",0
+     print(Name,Age,Qual,YoE)
+
+
+#10. Write a python script to print values of three variables, each in a new line. Variable contains integer values.
+Ans) a,b,c=1,2,3
+     print("%d\n%d\n%d" %(a,b,c))
